2d3b229bb735b9346eb417c411c3596d7a2a1864
dimahoperskiy <59148451+dimahoperskiy@users.noreply.github.com>
2020-06-04 00:37:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,14 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY&') or 'you-will-never-guess&#39'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:injustice@localhost/flask_mil'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 10
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = 1
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+    MAIL_USERNAME = 'flaskypassch@gmail.com'
+    MAIL_PASSWORD = 'flask_mil.py'
     LANGUAGES = ['en', 'ru']
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
